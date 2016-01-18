dae62a9495a6c5d27ea1fcfd6fa2d77d861faca7
Nan Wang <nantas@gmail.com>
2016-01-19 01:02:35 +0800

    add 4 dirs movement control.

---
+++ assets/scripts/Move.js
@@ -0,0 +1,94 @@
+const MoveState = cc.Enum({
+    Stand: -1,
+    Up: -1,
+    Right: -1,
+    Down: -1,
+    Left: -1
+});
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        moveSpeed: 0,
+        anim: {
+            default: null,
+            type: cc.Animation
+        }
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        this.moveState = MoveState.Stand;
+        this.node.on('move-up', this.moveUp, this);
+        this.node.on('move-right', this.moveRight, this);
+        this.node.on('move-down', this.moveDown, this);
+        this.node.on('move-left', this.moveLeft, this);
+        this.node.on('stand', this.stand, this);
+        this.node.on('update-dir', this.updateDir, this);
+    },
+
+    stand: function () {
+        if (this.moveState !== MoveState.Stand) {
+            this.anim.play('stand');
+            this.moveState = MoveState.Stand;
+        }
+    },
+
+    moveUp: function () {
+        if (this.moveState !== MoveState.Up) {
+            this.anim.play('run_up');
+            this.anim.node.scaleX = 1;
+            this.moveState = MoveState.Up;
+        }
+    },
+
+    moveDown: function () {
+        if (this.moveState !== MoveState.Down) {
+            this.anim.play('run_down');
+            this.anim.node.scaleX = 1;
+            this.moveState = MoveState.Down;
+        }
+    },
+
+    moveRight: function () {
+        if (this.moveState !== MoveState.Right) {
+            this.anim.play('run_right');
+            this.anim.node.scaleX = 1;
+            this.moveState = MoveState.Right;
+        }
+    },
+
+    moveLeft: function () {
+        if (this.moveState !== MoveState.Left) {
+            this.anim.play('run_right');
+            this.anim.node.scaleX = -1;
+            this.moveState = MoveState.Left;
+        }
+    },
+
+    updateDir: function (event) {
+        this.moveDir = event.detail.dir;
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    update: function (dt) {
+        if (this.moveDir) {
+            this.node.x += this.moveSpeed * this.moveDir.x * dt;
+            this.node.y += this.moveSpeed * this.moveDir.y * dt;
+            let deg = cc.radiansToDegrees(cc.pToAngle(this.moveDir));
+            console.log(deg);
+            if (deg >= 45 && deg < 135) {
+                this.moveUp();
+            } else if (deg >= 135 || deg < -135) {
+                this.moveLeft();
+            } else if (deg >= -45 && deg < 45) {
+                this.moveRight();
+            } else {
+                this.moveDown();
+            }
+        } else {
+            this.stand();
+        }
+    },
+});
--- assets/scripts/Player.js
+++ assets/scripts/Player.js
@@ -1,10 +1,3 @@
-const PlayerMove = cc.Enum({
-    MoveUp: -1,
-    MoveRight: -1,
-    MoveDown: -1,
-    MoveLeft: -1
-});
-
 cc.Class({
     extends: cc.Component,
 
@@ -12,6 +5,10 @@ cc.Class({
         fxTrail: {
             default: null,
             type: cc.ParticleSystem
+        },
+        spArrow: {
+            default: null,
+            type: cc.Node
         },
         sfAtkDirs: {
             default: [],
@@ -33,7 +30,35 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-        this.anim = this.getComponent(cc.Animation);
+        this.registerInput();
+        this.spArrow.active = false;
+    },
+
+    registerInput: function () {
+        var self = this;
+        // touch input
+        cc.eventManager.addListener({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            onTouchBegan: function(touch, event) {
+                var touchLoc = touch.getLocation();
+                self.isMoving = true;
+                self.moveToPos = self.node.parent.convertToNodeSpaceAR(touchLoc);
+                return true; // don't capture event
+            },
+            onTouchMoved: function(touch, event) {
+                var touchLoc = touch.getLocation();
+                self.spArrow.active = true;
+                self.moveToPos = self.node.parent.convertToNodeSpaceAR(touchLoc);
+            },
+            onTouchEnded: function(touch, event) {
+                self.isMoving = false; // when touch ended, stop moving
+                self.spArrow.active = false;
+                self.moveToPos = null;
+                self.node.emit('update-dir', {
+                    dir: null
+                });
+            }
+        }, self.node);
     },
 
     start: function () {
@@ -42,11 +67,19 @@ cc.Class({
 
     ready: function () {
         this.fxTrail.resetSystem();
-        this.anim.play('stand');
+        this.node.emit('stand');
     },
 
     // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
-
-    // },
+    update: function (dt) {
+        if (this.moveToPos) {
+            let dir = cc.pSub(this.moveToPos, this.node.position);
+            let rad = cc.pToAngle(dir);
+            let deg = cc.radiansToDegrees(rad);
+            this.spArrow.rotation = 90-deg;
+            this.node.emit('update-dir', {
+                dir: cc.pNormalize(dir)
+            });
+        }
+    },
 });
